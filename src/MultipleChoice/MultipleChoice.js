e3afb2d710cac6f60fe6a3f67d094dd9beb71439
Luther Karper <luther.karper@live.com>
2020-10-06 19:07:33 -0400

    Add MultipleChoice component

---
+++ src/MultipleChoice/MultipleChoice.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import QuestionDialogue from '../QuestionDialogue/QuestionDialogue';
+import QuestionLegend from '../QuestionLegend/QuestionLegend';
+import './MultipleChoice.css';
+
+const MultipleChoice = (props) => {
+    const { 
+        page, 
+        savedUserInput, 
+        checkAnswer, 
+    } = props;
+
+    const [userChoice, setChoice] = useState();
+    const [shuffledChoices, setShuffledChoices]= useState([]);
+
+    useEffect(() => {
+        const { 
+            correct_response, 
+            incorrect_response_option_1, 
+            incorrect_response_option_2, 
+            incorrect_response_option_3 
+        } = page;
+
+        const choicesArray = [
+            correct_response,
+            incorrect_response_option_1,
+            incorrect_response_option_2,
+            incorrect_response_option_3,
+        ].filter(choice => choice !== '');
+
+        const shuffledChoices = [];
+
+        while (choicesArray.length > 0) {
+            const randomNumber = Math.floor(Math.random() * choicesArray.length);
+            shuffledChoices.push(choicesArray[randomNumber]);
+            choicesArray.splice(randomNumber, 1);
+        }
+
+        setShuffledChoices(shuffledChoices);
+    }, [page]);
+
+    const choicesHTMLArray = shuffledChoices
+        .filter(choice => choice !== '')
+        .map((choice, i) => 
+            <div key={choice}>
+                <input 
+                    type="radio"
+                    id={`choice_${i}`}
+                    name='user-response'
+                    value={choice}
+                    checked={userChoice === choice}
+                    onChange={(e) => setChoice(e.target.value)}
+                    required
+                />
+                <label htmlFor={`choice_${i}`}>{choice}</label>
+            </div>
+        );
+
+    return (
+        <div className='MultipleChoice__container'>
+            <QuestionDialogue 
+                page={page} 
+                savedUserInput={savedUserInput} 
+            />
+            <form
+                onSubmit={(e) => {
+                    checkAnswer(e, userChoice);
+                    setChoice();
+                }}
+            >
+                <fieldset>
+                    <QuestionLegend 
+                        page={page} 
+                        savedUserInput={savedUserInput} 
+                    />
+                    {choicesHTMLArray}
+                </fieldset>
+                <button
+                    className='MultipleChoice__submit button' 
+                    type="submit"
+                    disabled={!userChoice}
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default MultipleChoice;
