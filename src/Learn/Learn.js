c98d8c31f4c92bc0100770bbf542efeccf832f5b
Luther Karper <luther.karper@live.com>
2020-10-06 18:32:33 -0400

    Add Learn component

---
+++ src/Learn/Learn.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import LearnPage from '../LearnPage/LearnPage';
+import STORE from '../STORE';
+import './Learn.css';
+
+const Learn = (props) => {
+    const { chapt } = props.match.params;
+
+    const [pages, setPages] = useState([]);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        const learnPages = STORE.learn_pages.filter(p => p.chapter_number === parseInt(chapt));
+        setPages(learnPages);
+    }, [chapt]);
+
+    return (
+        <>
+            {pages.length !== 0 
+                ? 
+                    <div className='Learn__container'>
+                        <LearnPage
+                            data={{
+                                pages,
+                                page,
+                                chapt,
+                                setPage,
+                            }}
+                        />
+                    </div>
+                : 
+                    <p className='Learn__loading'>Loading...</p>
+            }
+        </>
+    );
+}
+
+export default Learn;
