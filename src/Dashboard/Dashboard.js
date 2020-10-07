e0d8e2d521a5c20fc1cd4e4fed01477643c09d4d
Luther Karper <luther.karper@live.com>
2020-10-06 20:32:18 -0400

    Add Dashboard component

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NotesList from '../NotesList/NotesList';
+import ProgressReport from '../ProgressReport/ProgressReport';
+import './Dashboard.css';
+
+const Dashboard = () => {
+    return (
+        <section className='Dashboard__outer-section'>
+            <h2>Dashboard</h2>
+            <ProgressReport />
+            <NotesList suffix='dashboard' />
+        </section>
+    );
+}
+
+export default Dashboard;
